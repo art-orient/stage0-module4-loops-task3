6129f39ac90c89f700c5f6331b344b2bf9c12925
Aliaksandr Artsikhovich <art-orient@tut.by>
2022-07-10 22:55:20 +0300

    done

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int previous = 0;
+        int last = 1;
+        int current;
+        if (lastFibonacci > 0) {
+            System.out.println(0);
+        }
+        if (lastFibonacci > 1) {
+            System.out.println(1);
+        }
+        for (int i = 2; i < lastFibonacci; i++) {
+            current = previous + last;
+            System.out.println(current);
+            previous = last;
+            last = current;
+        }
     }
 }
